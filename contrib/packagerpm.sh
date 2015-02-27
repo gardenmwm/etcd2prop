1fccb3b23714edbf2ace849fa6890c6feb7522cb
gardenmwm <mm17@azdeq.gov>
2015-02-27 08:04:14 -0700

    Some changes

--- contrib/packagerpm.sh
+++ contrib/packagerpm.sh
@@ -4,4 +4,4 @@ echo "mkdir /var/cache/etcd2prop; chmod 777 /var/cache/etcd2prop" > /tmp/postins
 fpm -s dir -t rpm \
     -n etcd2prop -v 0.1 --iteration=0 \
     --rpm-user=root --description="Copy etcd store to java properties file" \
-    --after-install=/tmp/postinstall.sh ../etcd2prop.py=/usr/bin/etcd2prop
+    --after-install=postinstall.sh ../etcd2prop.py=/usr/bin/etcd2prop
